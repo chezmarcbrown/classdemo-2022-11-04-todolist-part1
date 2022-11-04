074c82ea2891d0004453dac2e4bd6477ea8788fe
chezmarcbrown <marc@chezbrown.org>
2022-11-03 16:19:39 -0800

    first commit

---
+++ todo_app/models.py
@@ -0,0 +1,18 @@
+# todo_list/todo_app/models.py
+
+from django.db import models
+from django.utils import timezone
+
+def one_week_hence():
+    return timezone.now() + timezone.timedelta(days=7)
+
+
+class ToDoList(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+class ToDoItem(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=one_week_hence)
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
